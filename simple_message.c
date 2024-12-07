053a6d4ffcc72da41c029f08c22811cf099d5c52
sameer-agarwal098 <sameeragarwal766@gmail.com>
2024-12-07 17:46:07 +0530

    Create simple_message.c
    Start the journey of c program.

---
+++ simple_message.c
@@ -0,0 +1,11 @@
+//Single Line Comment
+/*Multi Line Comment*/
+
+//Program to print simple message in c
+
+#include<stdio.h> //Header Fine include features like printf ,scanf and so no.
+
+void main() //Main function
+{
+    printf("\nHello World"); //Message
+}
